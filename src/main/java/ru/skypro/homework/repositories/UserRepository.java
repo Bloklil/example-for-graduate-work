3d8a06183008c4a88f737aba98ab77335f77e190
Alexandra <saa-saa@mail.ru>
2026-01-14 19:54:16 +0300

    Соколов_сущности, мапперы и репозитории

---
+++ src/main/java/ru/skypro/homework/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package ru.skypro.homework.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.skypro.homework.entities.UserEntity;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
