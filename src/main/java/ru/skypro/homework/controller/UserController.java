278666f75877d774ce94ab74a8ac09a510e2685d
Anton S <hitman.soldirs@gmail.com>
2026-01-22 21:17:18 +0800

    убрал глушки в контроллерах Comment и User

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -5,26 +5,36 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.UpdateUserDto;
 import ru.skypro.homework.dto.UserDto;
+import ru.skypro.homework.service.UserService;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @Operation(summary = "получить текущего user")
     @GetMapping("/me")
     public UserDto getUser() {
-        return new UserDto();
+        return userService.getCurrentUser();
     }
 
     @Operation(summary = "обновить user")
     @PatchMapping("/me")
-    public UpdateUserDto updateUser(@RequestBody UpdateUserDto dto) {
-        return dto;
+    public UserDto updateUser(@RequestBody UpdateUserDto dto) {
+        return userService.updateUser(dto);
     }
 
     @Operation(summary = "обновить avatar")
     @PatchMapping("/me/image")
-    public void updateImage(@RequestParam MultipartFile image) {
+    public void updateImage(@RequestParam MultipartFile image) throws IOException {
+        userService.updateUserImage(image);
     }
 
 }
